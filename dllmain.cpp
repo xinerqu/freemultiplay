f81b7702ad1ec62a2824794ff59a2ec5e1b3dd13
xinerqu <xinerqu@users.noreply.github.com>
2026-05-13 02:52:01 +0800

    Use .def forwarding instead of pragma, remove forward_pragmas.h

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,8 +5,6 @@
 
 #pragma comment(lib, "Shlwapi.lib")
 
-#include "forward_pragmas.h"
-
 // Type definitions (not provided by SDK headers in this project)
 typedef unsigned int uint32;
 typedef unsigned long long uint64;
